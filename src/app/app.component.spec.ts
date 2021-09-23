844403cacedd714d6ba83239e93d41b2cad2c17c
Pedrohero13 <pedrogadote@gmail.com>
2021-09-22 22:25:55 -0500

    update app.component.spect.ts

--- src/app/app.component.spec.ts
+++ src/app/app.component.spec.ts
@@ -26,10 +26,5 @@ describe('AppComponent', () => {
     expect(app.title).toEqual('ng5');
   });
 
-  it('should render title', () => {
-    const fixture = TestBed.createComponent(AppComponent);
-    fixture.detectChanges();
-    const compiled = fixture.nativeElement;
-    expect(compiled.querySelector('.content span').textContent).toContain('ng5 app is running!');
-  });
+  
 });
